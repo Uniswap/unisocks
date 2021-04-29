7b0363cf7e9d1638d403641b91ecef3cf22a2e99
Kaveer Kapoor <kaveerkapoor93@gmail.com>
2021-04-29 16:46:04 -0700

    chore: remove show loader logic

--- src/components/Web3ReactManager/index.js
+++ src/components/Web3ReactManager/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { useWeb3Context } from 'web3-react'
 import { ethers } from 'ethers'
 
@@ -29,21 +29,11 @@ export default function Web3ReactManager({ children }) {
     }
   }, [active, setConnector])
 
-  const [showLoader, setShowLoader] = useState(false)
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setShowLoader(true)
-    }, 750)
-    return () => {
-      clearTimeout(timeout)
-    }
-  }, [])
-
   if (error) {
     console.error(error)
     return <Message>Connection Error.</Message>
   } else if (!active) {
-    return showLoader ? <Message>Initializing...</Message> : null
+    return <Message>Initializing...</Message>
   } else {
     return children
   }
